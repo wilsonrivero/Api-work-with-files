dad46c49ce3b3ea82a0c023869c56b10254fe209
wilsonrivero <wilsonrivero2002@gmail.com>
2021-11-07 16:35:41 -0300

    flask CORS

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from src.config import SECRET_KEY
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///storage.db'
 app.secret_key = SECRET_KEY
 ma = Marshmallow(app)
+CORS(app)
 db = SQLAlchemy(app)
 
 
